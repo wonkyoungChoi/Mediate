474390cc37d2fc4f3bdf8dbb3b83933fffa233a4
wonkyoungChoi <72801820+wonkyoungChoi@users.noreply.github.com>
2022-02-02 15:25:57 +0900

    Mediate 프로젝트 6주차 - 학교 내부 인증코드 입력 부분 구현중(입력하면 다음버튼 활성화 구현, 다음페이지 넘어가기 구현해야함)

--- app/src/main/java/com/wk/mediate/ui/Register/RegisterInputCodeActivity.kt
+++ app/src/main/java/com/wk/mediate/ui/Register/RegisterInputCodeActivity.kt
@@ -3,6 +3,8 @@ package com.wk.mediate.ui.Register
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -18,6 +20,7 @@ class RegisterInputCodeActivity : AppCompatActivity() {
         binding = ActivityInputCodeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        editTextWatcher()
         focusableCode()
 
     }
@@ -27,7 +30,7 @@ class RegisterInputCodeActivity : AppCompatActivity() {
         binding.etCode.setOnFocusChangeListener { v, hasFocus ->
             if(hasFocus) {
                 Log.d("test","test")
-                binding.etCode.setPadding(50, 19, 50, 0)
+                binding.etCode.setPadding(50, 22, 50, 0)
                 binding.tvHintInputCode.visibility = View.VISIBLE
                 binding.etCode.hint = ""
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -36,9 +39,24 @@ class RegisterInputCodeActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkInput() {
-        //입력하면 다음 버튼 활성화
+    private fun editTextWatcher() {
+        binding.etCode.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.btNextActive.visibility = View.VISIBLE
+                binding.btNextInactive.visibility = View.GONE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(binding.etCode.text.isEmpty()) {
+                    binding.btNextActive.visibility = View.GONE
+                    binding.btNextInactive.visibility = View.VISIBLE
+                }
+            }
+
+        })
     }
-
-
 }
